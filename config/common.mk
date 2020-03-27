4ef841fa1e315c9e6ad76bc6176f76cc80cbe87a
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-03-27 16:55:56 -0300

    [2/4] Port face unlock feature
    Change-Id: I18dde37697ee4e68c9aa133da2fb71c3f03109bd

--- config/common.mk
+++ config/common.mk
@@ -210,3 +210,13 @@ PRODUCT_PACKAGES += \
 endif
 
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
+
+# Face Unlock
+TARGET_FACE_UNLOCK_SUPPORTED := false
+ifneq ($(TARGET_DISABLE_ALTERNATIVE_FACE_UNLOCK), true)
+PRODUCT_PACKAGES += \
+    FaceUnlockService
+TARGET_FACE_UNLOCK_SUPPORTED := true
+endif
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.face.moto_unlock_service=$(TARGET_FACE_UNLOCK_SUPPORTED)
